eefcd2b3369d595ebfc4003f2abba42313209b72
kanja23 <martinkaranja92@@gmail.com>
2023-09-01 08:52:30 +0000

    app.js

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const ejs = require('ejs');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
 
 app.get('/', (req, res) => {
 	res.render('index', { pageTitle: 'Home' });
